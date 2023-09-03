45e4dbea40982caf709b10e6afd304fa04b85145
Ethan Shalev <shalev.ethan@gmail.com>
2023-09-03 16:48:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generate_legoLink(userInput) {
-  const interface = document.getElementById("interface");
+  const output = document.getElementById("output");
   const url = `https://lego.com/en-us/scan?id=${userInput}&source=builder`;
-  interface.innerHTML = `<p>Generated URL:</p><a href="${url}" target="_blank">${url}</a>`;
+  output.innerHTML = `<p>Generated URL:</p><a href="${url}" target="_blank">${url}</a>`;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
